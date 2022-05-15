048d279f430014a847b500d042efae21a4ff1b5f
yahya banouk <yahya.banouk@uit.ac.ma>
2022-05-16 01:32:03 +0200

    last last version

--- app/src/main/java/com/example/mobapp_firebase/Profil.java
+++ app/src/main/java/com/example/mobapp_firebase/Profil.java
@@ -3,12 +3,15 @@ package com.example.mobapp_firebase;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Profil extends AppCompatActivity {
-
+    EditText etProfEmail,etProfPass,etProfName,etProfSuel,etProfCourse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profil);
+
+
     }
 }
